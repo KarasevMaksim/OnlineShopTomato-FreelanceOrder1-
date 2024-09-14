c23a01a5dd1cec27ef5ee3239c99572490f34cd6
KarasevMaksim <theallanartist@gmail.com>
2024-09-14 20:45:04 +0300

    add: empty login.html, fix __init__, base route url

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -4,5 +4,11 @@ from app.admin import bp
 from app import db, login
 
 
+@bp.route('/')
+def admin():
+    return render_template('admin/admin.html')
 
 
+@bp.route('/login')
+def login():
+    return render_template('admin/login.html')
