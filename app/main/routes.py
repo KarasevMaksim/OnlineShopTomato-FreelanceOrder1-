2da5af0b8cff83bf0249ec6e1d00289d70124da1
KarasevMaksim <theallanartist@gmail.com>
2024-09-28 20:36:54 +0300

    add cookies in filter

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,7 @@
-from flask import render_template, url_for, request, jsonify
+import json
+from flask import (
+    render_template, url_for, request, jsonify, make_response, redirect, abort
+)
 import sqlalchemy as sa
 from app.models import Products, Sections, SubSections
 from app.main import bp
@@ -9,6 +12,7 @@ from app.main.forms import (
 
 @bp.route('/', methods=['GET', 'POST'])
 def index():
+    title = 'Tomato'
     form = ShowProductsForm()
     sections_item = Sections.query.all()
     sections = [(i.name, i.name.capitalize()) for i in sections_item]
@@ -18,7 +22,10 @@ def index():
         ]
         form.select_section2.choices.extend(sections)
         form.select_sub_section2.choices.extend(sub_sections)
-    title = 'Tomato'
+    if request.cookies.get('user_data'):
+        print('\n YES')
+    else:
+        print('\n NO')
     products = Products.query.all()[::-1]
     return render_template(
         'index.html',
@@ -26,6 +33,38 @@ def index():
         products=products,
         form=form
     )
+
+
+@bp.route('/update-puduct', methods=['POST'])
+def update_product():
+    form = ShowProductsForm()
+    check_type_button = form.submit1.data
+    section = form.select_section2.data
+    sub_section = form.select_sub_section2.data
+
+    if section and sub_section and check_type_button:
+        user_data = {
+            'sections': section,
+            'sub_sections': sub_section
+        }
+        user_data_json = json.dumps(user_data)
+        response = make_response(
+            redirect(url_for('main.index'))
+        )
+        response.set_cookie(
+            'user_data',
+            user_data_json,
+            max_age=60*60*24
+        )
+        return response
+
+    elif request.cookies.get('user_data') and not check_type_button:
+        response = make_response(redirect(url_for('main.index')))
+        response.delete_cookie('user_data')
+        return response
+
+    return abort(500)
+
 
 @bp.route('/product-filter', methods=['POST'])
 def product_filter():
