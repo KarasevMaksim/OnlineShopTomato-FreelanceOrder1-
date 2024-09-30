ef2ccad8a75f1c8ccb5d592586819ca6887eeadb
KarasevMaksim <theallanartist@gmail.com>
2024-10-01 00:23:44 +0300

    update add_to_basket

--- app/slug/routes.py
+++ app/slug/routes.py
@@ -22,6 +22,24 @@ def index(id):
 
 @bp.route('/add-to-basket', methods=['POST'])
 def add_to_basket():
+    response = make_response(redirect(url_for('slug.index', id=product_id)))
     product_id = request.form.get('get_id')
+    cost = request.form.get('cost')
+    if product_id and cost:
+        data_basket_json = request.cookies.get('data_basket')
+        if data_basket_json:
+            data_basket = json.loads(data_basket_json)
+            if product_id in data_basket and data_basket[product_id] != cost:
+                data_basket[product_id] = cost
+                
+                data_basket_json = json.dumps(data_basket)
+                response.set_cookie(
+                    'data_basket',
+                    data_basket_json,
+                    max_age=60*60*24    
+                )
+                flash("Товар успешно добавлен в корзину!")
+                return response
+                
     flash("Товар успешно добавлен в корзину!")
     return redirect(url_for('slug.index', id=product_id))
