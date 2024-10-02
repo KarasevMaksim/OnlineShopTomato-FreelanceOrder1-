73d4f58b525fdc932e2629e67370465c6510c1ea
KarasevMaksim <theallanartist@gmail.com>
2024-10-02 23:40:04 +0300

    add parse product in index route

--- app/basket/routes.py
+++ app/basket/routes.py
@@ -10,4 +10,16 @@ from app.basket import bp
 
 @bp.route('/')
 def index():
-    return render_template('basket/basket.html')
+    data_basket = request.cookies.get('data_basket')
+    if data_basket:
+        data_basket = json.loads(data_basket)
+        basket_items = list(data_basket.items())
+        products_and_count = [
+            (Products.query.get(int(i[0])), i[1]) for i in basket_items
+        ]
+    
+    return render_template(
+        'basket/basket.html',
+        products_and_count=products_and_count
+    )
+    
