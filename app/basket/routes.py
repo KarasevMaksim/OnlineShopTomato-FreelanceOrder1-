fab69c3bf5c796fa6e027a08a4bd14e1814e062c
KarasevMaksim <theallanartist@gmail.com>
2024-10-03 00:30:36 +0300

    fix index and add delete products in basket

--- app/basket/routes.py
+++ app/basket/routes.py
@@ -10,6 +10,7 @@ from app.basket import bp
 
 @bp.route('/')
 def index():
+    products_and_count = list()
     data_basket = request.cookies.get('data_basket')
     if data_basket:
         data_basket = json.loads(data_basket)
@@ -23,3 +24,23 @@ def index():
         products_and_count=products_and_count
     )
     
+    
+@bp.route('/delete-product', methods=['POST'])
+def delete_product():
+    response = make_response(redirect(url_for('basket.index')))
+    data_id = request.form.get('get_id')
+    data_basket_json = request.cookies.get('data_basket')
+    if data_basket_json:
+        data_basket = json.loads(data_basket_json)
+        if len(data_basket.keys()) == 1:
+            response.delete_cookie('data_basket')
+        else:
+            del data_basket[data_id]
+            data_basket_json = json.dumps(data_basket)
+            response.set_cookie(
+                'data_basket',
+                data_basket_json,
+                max_age=60*60*24
+            )
+        return response
+    return redirect(url_for('basket.index'))
