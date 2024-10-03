1fac91db2fbf21f22e025a65c72f93ab19ab2dcb
KarasevMaksim <theallanartist@gmail.com>
2024-10-04 00:24:14 +0300

    add final price

--- app/static/js/price_counts_in_basket.js
+++ app/static/js/price_counts_in_basket.js
@@ -1,10 +1,12 @@
 const quantityInputs = document.querySelectorAll('.quantity');
 const totalCostElements = document.querySelectorAll('.total-cost-value');
 const priceElements = document.querySelectorAll('.price-element');
+const totalCostAllProductsElement = document.getElementById('total-cost-all-products-value');
 
 quantityInputs.forEach((input, index) => {
   input.addEventListener('input', () => {
     updateTotalCost(index);
+    updateTotalCostAllProducts();
   });
 });
 
@@ -14,3 +16,14 @@ function updateTotalCost(index) {
   const totalCost = quantity * productPrice;
   totalCostElements[index].textContent = `${totalCost} р.`;
 }
+
+function updateTotalCostAllProducts() {
+  let totalCostAllProducts = 0;
+  totalCostElements.forEach((element) => {
+    const totalCost = parseInt(element.textContent.replace(' р.', ''));
+    totalCostAllProducts += totalCost;
+  });
+  totalCostAllProductsElement.textContent = `${totalCostAllProducts} р.`;
+}
+
+updateTotalCostAllProducts();
