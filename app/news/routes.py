19b8cb4ceae33d52f4ec83dd698a7af10000aae8
KarasevMaksim <theallanartist@gmail.com>
2024-10-04 10:17:05 +0300

    add routes in news

--- app/news/routes.py
+++ app/news/routes.py
@@ -1,13 +1,17 @@
-import json
-
 from flask import (
     render_template, url_for, request, jsonify, make_response, redirect, abort,
     flash
 )
-from app.models import Products, Sections, SubSections
+from app.models import News
 from app.news import bp
 
 
 @bp.route('/')
 def index():
-    return render_template('news/news.html')
+    news = News.query.all()
+    print(news[0].post)
+    return render_template(
+        'news/news.html',
+        news=news
+    )
+    
