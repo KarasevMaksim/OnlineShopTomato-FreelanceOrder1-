9210637309d1bedfd9e6a7b4e19e8a0943b35810
KarasevMaksim <theallanartist@gmail.com>
2024-10-06 18:19:10 +0300

    add cofigurate for mail send feature

--- config.py
+++ config.py
@@ -5,3 +5,9 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     FLASK_DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
