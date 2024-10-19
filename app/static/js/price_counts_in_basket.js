ab41f4d7e28bda9d9f30aedd92ea7038a84fba70
KarasevMaksim <theallanartist@gmail.com>
2024-10-20 01:17:42 +0300

    feat: Add loading animation support for form button
    - Added a hidden button with a loading indicator in `basket.html`.
    - Implemented a new function in `price_counts_in_basket.js` that toggles
      the display of the static button to the animated button upon form
      submission.

--- app/static/js/price_counts_in_basket.js
+++ app/static/js/price_counts_in_basket.js
@@ -27,3 +27,9 @@ function updateTotalCostAllProducts() {
 }
 
 updateTotalCostAllProducts();
+
+function handleSubmit(event) {
+      document.getElementById("submitButton").style.display = "none";
+      document.getElementById("loadingButton").style.display = "block";
+      document.getElementById("myForm").submit();
+    }
