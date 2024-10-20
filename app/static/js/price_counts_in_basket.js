4ee9529d45dae1b185d22f8d8a649bd33c92ad84
KarasevMaksim <theallanartist@gmail.com>
2024-10-20 14:56:35 +0300

    refactor: Moved handleSubmit function into file
    - Transferred the handleSubmit(event) function code from the
      price_counts_in_basket.js file to the new anim_button.js file.
    - Linked the function from anim_button.js through
      basket/_scripts.html.
    - Improved code modularity, allowing the function to be used in
      other parts of the program.

--- app/static/js/price_counts_in_basket.js
+++ app/static/js/price_counts_in_basket.js
@@ -28,8 +28,4 @@ function updateTotalCostAllProducts() {
 
 updateTotalCostAllProducts();
 
-function handleSubmit(event) {
-      document.getElementById("submitButton").style.display = "none";
-      document.getElementById("loadingButton").style.display = "block";
-      document.getElementById("myForm").submit();
-    }
+
